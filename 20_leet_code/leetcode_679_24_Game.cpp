71fd16e28d1b9da768f16b743e5a83c7a51b3090
Jackie Pang <jackiepang15@gmail.com>
2026-05-08 00:24:28 +0800

    print results

--- 20_leet_code/leetcode_679_24_Game.cpp
+++ 20_leet_code/leetcode_679_24_Game.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
-#include <numeric>
+#include <string>
+#include <set>
+#include <cmath>
 #include <algorithm>
 
 using namespace std;
@@ -9,80 +11,91 @@ class Solution
 {
     const double EPS = 1e-6;
 
-    // 尝试对 a 和 b 进行 6 种运算，结果存入 res
-    // 返回生成的有效结果数量
-    inline int get_ops(double a, double b, double res[])
+    struct Node
     {
-        res[0] = a + b;
-        res[1] = a - b;
-        res[2] = b - a;
-        res[3] = a * b;
-        int count = 4;
-        if (abs(b) > EPS)
-            res[count++] = a / b;
-        if (abs(a) > EPS)
-            res[count++] = b / a;
-        return count;
-    }
+        double val;
+        string expr;
+    };
 
-    bool check(double nums[])
+    // generate all possible operations between two nodes
+    void get_ops(const Node &a, const Node &b, vector<Node> &res)
     {
-        double a = nums[0], b = nums[1], c = nums[2], d = nums[3];
-        double res1[6], res2[6], res3[6];
-
-        // --- 拓扑 A: ((a op b) op c) op d ---
-        int cnt1 = get_ops(a, b, res1);
-        for (int i = 0; i < cnt1; ++i)
-        {
-            int cnt2 = get_ops(res1[i], c, res2);
-            for (int j = 0; j < cnt2; ++j)
-            {
-                int cnt3 = get_ops(res2[j], d, res3);
-                for (int k = 0; k < cnt3; ++k)
-                {
-                    if (abs(res3[k] - 24) < EPS)
-                        return true;
-                }
-            }
-        }
-
-        // --- 拓扑 B: (a op b) op (c op d) ---
-        // 注意：由于 next_permutation 会遍历所有排列，
-        // 我们只需要检查 (a op b) 和 (c op d) 的组合即可覆盖所有情况
-        cnt1 = get_ops(a, b, res1);
-        int cnt_pair = get_ops(c, d, res2);
-        for (int i = 0; i < cnt1; ++i)
-        {
-            for (int j = 0; j < cnt_pair; ++j)
-            {
-                int cnt3 = get_ops(res1[i], res2[j], res3);
-                for (int k = 0; k < cnt3; ++k)
-                {
-                    if (abs(res3[k] - 24) < EPS)
-                        return true;
-                }
-            }
-        }
-        return false;
+        res.push_back({a.val + b.val, "(" + a.expr + "+" + b.expr + ")"});
+        res.push_back({a.val - b.val, "(" + a.expr + "-" + b.expr + ")"});
+        res.push_back({b.val - a.val, "(" + b.expr + "-" + a.expr + ")"});
+        res.push_back({a.val * b.val, "(" + a.expr + "*" + b.expr + ")"});
+        if (abs(b.val) > EPS)
+            res.push_back({a.val / b.val, "(" + a.expr + "/" + b.expr + ")"});
+        if (abs(a.val) > EPS)
+            res.push_back({b.val / a.val, "(" + b.expr + "/" + a.expr + ")"});
     }
 
 public:
     bool judgePoint24(vector<int> &nums)
     {
-        // 先排序以配合 next_permutation
+        set<string> solutions; // used for result deduplication
         sort(nums.begin(), nums.end());
-        double d_nums[4];
-        for (int i = 0; i < 4; ++i)
-            d_nums[i] = (double)nums[i];
 
-        // 遍历 4! = 24 种全排列
         do
         {
-            if (check(d_nums))
-                return true;
-        } while (next_permutation(d_nums, d_nums + 4));
+            vector<Node> d(4);
+            for (int i = 0; i < 4; ++i)
+                d[i] = {(double)nums[i], to_string(nums[i])};
 
-        return false;
+            // --- Topology A: ((a op b) op c) op d ---
+            vector<Node> res1, res2, res3;
+            get_ops(d[0], d[1], res1);
+            for (auto &n1 : res1)
+            {
+                res2.clear();
+                get_ops(n1, d[2], res2);
+                for (auto &n2 : res2)
+                {
+                    res3.clear();
+                    get_ops(n2, d[3], res3);
+                    for (auto &n3 : res3)
+                    {
+                        if (abs(n3.val - 24) < EPS)
+                            solutions.insert(n3.expr);
+                    }
+                }
+            }
+
+            // --- Topology B: (a op b) op (c op d) ---
+            res1.clear();
+            vector<Node> res_pair;
+            get_ops(d[0], d[1], res1);
+            get_ops(d[2], d[3], res_pair);
+            for (auto &nL : res1)
+            {
+                for (auto &nR : res_pair)
+                {
+                    res3.clear();
+                    get_ops(nL, nR, res3);
+                    for (auto &n3 : res3)
+                    {
+                        if (abs(n3.val - 24) < EPS)
+                            solutions.insert(n3.expr);
+                    }
+                }
+            }
+        } while (next_permutation(nums.begin(), nums.end()));
+
+        // print results
+        if (solutions.empty())
+        {
+            cout << "No solution found." << endl;
+            return false;
+        }
+        else
+        {
+            cout << "Find " << solutions.size() << " different solutions:" << endl;
+            for (const string &s : solutions)
+            {
+                cout << s << " = 24" << endl;
+            }
+        }
+        return true;
     }
 };
 
@@ -98,6 +111,5 @@ int main(int argc, char *argv[])
     }
     Solution solution;
     bool result = solution.judgePoint24(nums);
-    cout << "Can the numbers " << nums[0] << ", " << nums[1] << ", " << nums[2] << ", " << nums[3] << " be used to get 24? " << (result ? "Yes" : "No") << endl;
     return 0;
 }
